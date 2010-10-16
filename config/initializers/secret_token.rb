2b80ed3db3adff82cb87d37e5c98ed9658aab06b
murrcs <x.eshan@hotmail.com>
2010-10-16 23:06:18 +0500

    rails3 architecture

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Murrcs::Application.config.secret_token = 'a8b50f2e6d2c39e6cc563d7a5a1a3ad7723be38e502dfc115dd6fb8e5d7e1f4a8cb36f498f29d7f53900989f2d72e9261382dfab882c3a4a471042aecac4c343'
