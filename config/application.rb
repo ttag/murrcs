1901eb1516226adf427d0390a57bbe947ad73b8a
x.eshan@hotmail.com <x.eshan@hotmail.com>
2010-10-20 22:28:52 +0500

    :: added jquery date picker ::
    - added ujs driver
    
     -- remove everything from public/javascripts/*
     --git submodule add git://github.com/rails/jquery-ujs.git public/javascripts/jquery-ujs
     --git submodule init
     --git submodule update
     -- added following in application.html.erb
           javascript_include_tag :defaults
     -- added following in application.rb
    	config.action_view.javascript_expansions[:defaults] = ['jquery-1.4.3.min.js', 'jquery-ujs/src/rails']
     -- copied jquery-1.4.3.min.js to public/javascripts
    
    - copied anytime.js to public/javascripts and added anytime.js as javascript_include_tag in application.html.erb
    - added code of AnyTime_picker in application.html.erb
    - added field in form
        <%= f.text_field :start_date, :id => "Calendar" %>
    - copied anytime.css in stylesheets folder
    
    thats it

--- config/application.rb
+++ config/application.rb
@@ -38,5 +38,7 @@ module Murrcs
 
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
+
+config.action_view.javascript_expansions[:defaults] = ['jquery-1.4.3.min.js', 'jquery-ujs/src/rails']
   end
 end
