3b243ce49d84d1cee1259af134df2a9cea7f3440
x.eshan@hotmail.com <x.eshan@hotmail.com>
2010-10-30 22:43:32 +0500

    - added login at signup screen - applied validate uniqueness for login

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,15 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable, :lockable and :timeoutable
-  devise :database_authenticatable, :registerable,
+  validates_presence_of :login
+  validates_uniqueness_of :login
+  devise :database_authenticatable, :registerable,:registerable,
          :recoverable, :rememberable, :trackable, :validatable,
          :token_authenticatable, :confirmable, :lockable, :timeoutable
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me, :role_id, :project_id, :login 
+
 
   devise :database_authenticatable, :authentication_keys => [:login]
   has_one :roles, :class_name => "Roles" 
