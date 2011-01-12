eb808a0e6d1c838f8194a6dab40042a27e127792
x.eshan@hotmail.com <x.eshan@hotmail.com>
2011-01-13 00:06:32 +0500

    fresh arch

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Murrcs::Application.config.secret_token = 'c2df451215e127866ee2f62edc0f04bf9b10b70a8b5c80c755571e4669fef8684201ff96bf02aa1d3e23cccd91943fac4f426a4e9623870850475c23aef379b5'
